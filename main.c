cabcfe1619b0532f496bcf0f65d9335660f7e61f
minjiwkim <minji5489235790@gmail.com>
2023-11-05 22:37:16 +0900

    2

--- main.c
+++ main.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #define N_STUDENT  5
 
 int main(int argc, char *argv[])
 {
-    int grade[N_STUDENT];
     int i;
-    
-    grade[0] = 10;
-    grade[1] = 20;
-    grade[2] = 30;
-    grade[3] = 40;
-    grade[4] = 50;
+    int grade[N_STUDENT];
+    printf("input 5 scores: ");
     
     for(i=0;i<N_STUDENT;i++)
-    printf("grade[%i] = %i\n", i, grade[i]);
+    scanf("%d", &grade[i]);
+    
+    for(i=0;i<N_STUDENT;i++)
+    printf("grade[%d] = %d\n", i, grade[i]);
   
   system("PAUSE");	
   return 0;
